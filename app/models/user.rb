36fbd571e678a89510548b5947e95d743b63a9b4
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-12-09 16:25:22 +0900

    Userモデルにdependentを記述追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :posts
-  has_many :reservations
+  has_many :posts, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
