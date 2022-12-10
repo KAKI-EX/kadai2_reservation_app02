0df1d096ff060a2a0454558fd1c95ca4d38d2d4f
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-12-10 19:56:02 +0900

    userprofileを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_02_121149) do
+ActiveRecord::Schema.define(version: 2022_12_10_090020) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2022_12_02_121149) do
     t.integer "total_fee", null: false
   end
 
+  create_table "userprofiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "self_introduce", default: ""
+    t.string "user_name", default: ""
+    t.string "user_avatar", default: ""
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_12_02_121149) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "self_introduce"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
